96f7895273ec4031acf41335afa39377965dc287
aakanshakansal01 <aakanshakansal5@gmail.com>
2024-05-23 12:30:45 +0530

    second commit

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1fdogyfLicqyafjLxI55dmxCDzir0jePDDqxKcuGj';
+const appId = 'com.gmail.prajapatisatya95.microverse';
+export default  {apiKey, appId};
